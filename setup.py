3fb037bbb457a83cb805bcc7b56c7cda02429546
mrtommyb <tom@tombarclay.com>
2014-09-26 11:05:42 -0700

    bumping version number

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
     setup
 
 setup(name='K2fov',
-      version='1.4.0',
+      version='1.4.1',
       description='Find which targets are in the field of view of K2',
       author='Tom Barclay',
       author_email='tom@tombarclay.com',
