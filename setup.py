31a21957653b90a43c5046d62318fb27200af322
Tom Barclay <tom@tombarclay.com>
2015-10-31 11:39:45 -0700

    bumping version humber

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ except ImportError:
     setup
 
 setup(name='K2fov',
-      version='1.8.0',
+      version='1.9.0',
       description='Find which targets are in the field of view of K2',
       author='Tom Barclay',
       author_email='tom@tombarclay.com',
