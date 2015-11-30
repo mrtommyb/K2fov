bd7e2fe3f89774c66571d43afa36eafb3dc6add2
Geert Barentsen <geert@barentsen.be>
2015-11-29 19:33:04 -0800

    pep8 + code improvements

--- K2fov/__init__.py
+++ K2fov/__init__.py
@@ -1,10 +1,9 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
 from __future__ import (division, absolute_import)
 
+# Where are the K2 Campaign parameters stored?
 import os
 PACKAGEDIR = os.path.dirname(os.path.abspath(__file__))
-CAMPAIGN_DICT_FILE = os.path.join(PACKAGEDIR, "data", "k2-campaigns.json")
+CAMPAIGN_DICT_FILE = os.path.join(PACKAGEDIR, "data",
+                                  "k2-campaign-parameters.json")
 
 from .K2onSilicon import K2onSilicon
