2397b36584de3b0ef5b5896692df5fae7d0bc731
Geert Barentsen <geert@barentsen.be>
2015-12-26 23:28:12 +0100

    make runK2onSilicon call K2onSilicon_main (avoids code duplication)

--- runK2onSilicon.py
+++ runK2onSilicon.py
@@ -1,28 +1,12 @@
 #! /usr/bin/env python
-
-### a simple script that will call K2fov
-from __future__ import division, print_function
+# A simple script that will call the K2fov.K2onSilicon() function
 import sys
 
 try:
-    import K2fov
+    from K2fov.K2onSilicon import K2onSilicon_main
 except ImportError:
     print('You need K2fov installed: pip install K2fov')
     sys.exit(1)
 
-import argparse
-
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='Run K2onSilicon to find which targets in a \
-        list call on active silicon for a given K2 campaign.')
-    parser.add_argument('csv_file',
-        help='Name of input csv file with targets, column are \
-        Ra_degrees, Dec_degrees, Kepmag', type=str)
-    parser.add_argument('campaign',
-        help='K2 Campaign number', type=int)
-    args = parser.parse_args()
-
-    K2fov.K2onSilicon(args.csv_file,args.campaign)
-
-
+    K2onSilicon_main()
