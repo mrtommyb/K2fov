146ed8ea7eea45ab5e93ee8f3776499c77c5eea0
Geert Barentsen <geert@barentsen.be>
2015-12-30 14:04:47 -0800

    Resolve merge conflict

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ except ImportError:
 # Command-line tools
 entry_points = {'console_scripts': [
     'K2onSilicon = K2fov.K2onSilicon:K2onSilicon_main',
-    'K2inMicrolensRegion = K2fov.c9:inMicrolensRegion_main'
+    'K2inMicrolensRegion = K2fov.c9:inMicrolensRegion_main',
     'K2findCampaigns = K2fov.K2findCampaigns:K2findCampaigns_main',
     'K2findCampaigns-csv = K2fov.K2findCampaigns:K2findCampaigns_csv_main'
 ]}
