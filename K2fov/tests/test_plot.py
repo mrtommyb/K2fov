336faefa8455c81ac0d0b90d089f9d281d44273f
Geert Barentsen <geert@barentsen.be>
2016-01-04 13:44:40 -0800

    Adds a unit test for the plot module

---
+++ K2fov/tests/test_plot.py
@@ -0,0 +1,9 @@
+"""Tests K2fov.plot"""
+
+from .. import plot
+
+
+def test_basics():
+    """Make sure this runs without exception."""
+    plot.create_context_plot(180, 0)
+    plot.create_context_plot_zoomed(180, 0)
