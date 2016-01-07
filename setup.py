6383dd9bd92c40f1da8aeac86dba5b3fef83bbc1
Geert Barentsen <geert@barentsen.be>
2016-01-07 00:26:03 -0500

    Bugfix: use scripts instead of entry_points

--- setup.py
+++ setup.py
@@ -13,23 +13,15 @@ except ImportError:
     from distutils.core import setup
     setup
 
-
-# Command-line tools
-entry_points = {'console_scripts': [
-    'K2onSilicon = K2fov.K2onSilicon:K2onSilicon_main',
-    'k2onsilicon = K2fov.K2onSilicon:K2onSilicon_main',
-    'K2inMicrolensRegion = K2fov.c9:inMicrolensRegion_main',
-    'k2inmicrolensregion = K2fov.c9:inMicrolensRegion_main',
-    'K2findCampaigns = K2fov.K2findCampaigns:K2findCampaigns_main',
-    'k2findcampaigns = K2fov.K2findCampaigns:K2findCampaigns_main',
-    'K2findCampaigns-byname = K2fov.K2findCampaigns:K2findCampaigns_byname_main',
-    'k2findcampaigns-byname = K2fov.K2findCampaigns:K2findCampaigns_byname_main',
-    'K2findCampaigns-csv = K2fov.K2findCampaigns:K2findCampaigns_csv_main',
-    'k2findcampaigns-csv = K2fov.K2findCampaigns:K2findCampaigns_csv_main'
-]}
+# Command-line tools; we're not using "entry_points" for now due to
+# a bug in pip which turns all the tools into lowercase
+scripts = ['scripts/K2onSilicon',
+           'scripts/K2findCampaigns',
+           'scripts/K2findCampaigns-byname',
+           'scripts/K2findCampaigns-csv']
 
 setup(name='K2fov',
-      version='3.0.2',
+      version='3.0.3',
       description='Find which targets are in the field of view of K2',
       author='Fergal Mullally, Tom Barclay, Geert Barentsen',
       author_email='tom@tombarclay.com',
@@ -37,7 +29,7 @@ setup(name='K2fov',
       packages=['K2fov'],
       package_data={'K2fov': ['data/*.json']},
       install_requires=["numpy>=1.8"],
-      entry_points=entry_points,
+      scripts=scripts,
       classifiers=[
           "Development Status :: 5 - Production/Stable",
           "License :: OSI Approved :: MIT License",
