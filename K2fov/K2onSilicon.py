3e6d691a23d944e646a7f92313b671f12fdf4745
Geert Barentsen <geert@barentsen.be>
2017-02-24 11:24:59 -0800

    Add docstrings

--- K2fov/K2onSilicon.py
+++ K2fov/K2onSilicon.py
@@ -84,19 +84,23 @@ def parse_file(infile, exit_on_error=True):
 
 
 def onSiliconCheck(ra_deg, dec_deg, FovObj, padding_pix=DEFAULT_PADDING):
+    """Check a single position."""
     dist = angSepVincenty(FovObj.ra0_deg, FovObj.dec0_deg, ra_deg, dec_deg)
     if dist >= 90.:
         return False
     # padding_pix=3 means that objects less than 3 pixels off the edge of
     # a channel are counted inside, to account for inaccuracies in K2fov.
     return FovObj.isOnSilicon(ra_deg, dec_deg, padding_pix=padding_pix)
-    
+
+
 def onSiliconCheckList(ra_deg, dec_deg, FovObj, padding_pix=DEFAULT_PADDING):
-    dist = angSepVincenty(FovObj.ra0_deg, FovObj.dec0_deg, ra_deg, dec_deg)    
+    """Check a list of positions."""
+    dist = angSepVincenty(FovObj.ra0_deg, FovObj.dec0_deg, ra_deg, dec_deg)
     mask = (dist < 90.)
     out = np.zeros(len(dist), dtype=bool)
     out[mask] = FovObj.isOnSiliconList(ra_deg[mask], dec_deg[mask], padding_pix=padding_pix)
     return out
+
 
 def nearSiliconCheck(ra_deg, dec_deg, FovObj, max_sep=8.2):
     dist = angSepVincenty(FovObj.ra0_deg, FovObj.dec0_deg, ra_deg, dec_deg)
